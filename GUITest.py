cfe3b9fd9dde935dc41df7c9c130c6fa926481dd
YasiTL <yasaman.most@gmail.com>
2021-04-26 22:20:43 -0500

    traverse app on button press

--- GUITest.py
+++ GUITest.py
@@ -1,28 +1,64 @@
 import tkinter as tk
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
+class RootApp:
+    def __init__(self, root=None):
+        self.root = root
+        self.frame = tk.Frame(self.root)
+        self.frame.pack()
+
+        self.root.minsize(600,600)
+        self.root.maxsize(600,600)
+
+        tk.Label(self.frame, text='Sign In').pack()
+        # tk.Entry(root, text = 'Username').pack()
+        # tk.Entry(root, text = 'Password').pack()
+        tk.Button(self.frame, text='Login', command=self.make_Menu).pack()
+
+        self.menuPage = Menu(master=self.root, app=self)
+
+    def main_page(self):
+        self.frame.pack()
+
+    def make_Menu(self):
+        self.frame.pack_forget()
+        self.menuPage.start()
+
+class Menu(tk.Frame):
+    def __init__(self, master=None, app=None):
+        # Set master, app, and frame
         self.master = master
-        # Set minimum size for application
-        self.master.minsize(600,600)
-        self.master.maxsize(600,600)
-        self.pack()
-        self.create_widgets()
+        self.app = app
+        self.frame = tk.Frame(self.master)
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        tk.Label(self.frame, text='Page 1').pack()
+        tk.Button(self.frame, text='Edit User', command=self.make_EditUser).pack()
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        # Menu Components
+        self.editUser = EditUser(master=self.master, app=self.app)
+        # self.reports = Reports(master=self.root, app=self)
 
-    def say_hi(self):
-        print("hi there, everyone!")
+    def start(self):
+        self.frame.pack()
+
+    def make_EditUser(self):
+        self.frame.pack_forget()
+        self.editUser.start()
+
+
+class EditUser(tk.Frame):
+    def __init__(self, master=None, app=None):
+        # Set master, app, and frame
+        self.master = master
+        self.app = app
+        self.frame = tk.Frame(self.master)
+
+        tk.Label(self.frame, text="Edit User Page").pack(side="top", fill="x", pady=10)
+
+    def start(self):
+        self.frame.pack()
+
+
 
 root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+app = RootApp(root=root)
+root.mainloop()
