a043274638a519b551d0226fd9d73c64a586c380
YasiTL <yasaman.most@gmail.com>
2021-04-26 19:45:56 -0500

    create new window

--- GUITest.py
+++ GUITest.py
@@ -1,2 +1,20 @@
-import tkinter
-tkinter._test()
+from tkinter import * 
+from tkinter.ttk import *
+    
+# writing code needs to
+# create the main window of 
+# the application creating 
+# main window object named root
+root = Tk()
+  
+# giving title to the main window
+root.title("Employee Project")
+  
+# Label is what output will be 
+# show on the window
+label = Label(root, text ="Hello World !").pack()
+
+# calling mainloop method which is used
+# when your application is ready to run
+# and it tells the code to keep displaying 
+root.mainloop()
