ac13f24aa6574c2e19a4b4a3137fd272fedafa70
YasiTL <yasaman.most@gmail.com>
2021-04-26 20:50:35 -0500

    basic sql abstraction

--- connTest.py
+++ connTest.py
@@ -5,13 +5,13 @@ from enum import Enum
 
 
 class Entity(Enum):
-    STATE = "state_t"
-    EMPLOYEE = "employee_t"
-    INSURNACE_PLAN = "insurancePlan_t"
-
+    STATE = ("state_t", "state_name, tax_rate")
+    INSURNACE_PLAN = ("insurancePlan_t", "plan_id, employee_cost_for_individualPlan, employee_cost_for_familyPlan, employer_cost_for_indivudal, employer_cost_for_family")
+    EMPLOYEE = ("employee_t", "e_id, first_name, last_name, ssn, job_title, salary_type, insurancePlan, email, country, state, street_name, postal_code, F01k_deduction")
 
 class Query(Enum):
-    CREATE = "insert into {}(state_name, tax_rate) values ('test2', 144);"
+    CREATE = "insert into {0}({1}) values {2};"
+    # UPDATE = "update {0} set {2} where {3} returning *;"
 
 
 class PostGresDB:
@@ -25,18 +25,30 @@ class PostGresDB:
 
     def exec(self, sql: str):
         self.cur.execute(sql)
+        self.conn.commit()
 
     def result(self):
-        return self.cur.fetchall()
+        try:
+            return self.cur.fetchall()
+        except psycopg.ProgrammingError as e:
+            return e
 
-    def runQuery(self, query: Query, entity: Entity):
-        self.exec(query.value.format(entity.value))
+    def runQuery(self, query: Query, entity: Entity, *args):
+        sql = query.value.format(entity.value[0], entity.value[1], str(args))
+        print("SQL: ", sql)
+        self.exec(sql)
+        print(db.result())
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
 
 
 def main():
     db = PostGresDB(sys.argv[1], "Payroll")
-    db.runQuery(Query.CREATE, Entity.EMPLOYEE)
-    print(db.result())
+    db.runQuery(Query.CREATE, Entity.EMPLOYEE, 'TestID2', 'boi', 'Smith', 4201337, 'ADMIN', 'HOURLY', 'basic health', 'TechYeah@iit.edu', 'Albania', 'Illinois', 'Main Street', 1808, 25.45)
+    # db.runQuery(Query.UPDATE, Entity.EMPLOYEE, )
+    db.close()
 
 
 main()
