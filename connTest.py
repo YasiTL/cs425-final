b68f158ddd8501caf466a330d696d60a1847b74d
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-05-02 16:09:15 -0500

    imports

--- connTest.py
+++ connTest.py
@@ -1,5 +1,5 @@
 import sys
-from postGres import PostGresDB, Query, Entity
+from postGres import PostGresDB, Query, Entity, Multivalue, Relation
 
 
 def main():
@@ -7,12 +7,13 @@ def main():
         exit("No ip given as argument")
 
     db = PostGresDB(sys.argv[1], "Payroll")
-    db.exec(Query.DELETE(Entity.STATE, "state_name='bongoStated'"))
-    db.exec(Query.CREATE(Entity.STATE, "bongoStated", "456"))
-    db.exec(Query.UPDATE(Entity.STATE, "state_name='bongoStated'", "tax_rate=753.12"))
-    db.exec(Query.SELECT(Entity.INSURNACE_PLAN, "*"))
-    db.exec(Query.SELECT(Entity.EMPLOYEE, "first_name", "last_name", "job_title"))
-    db.exec(Query.CREATE(Entity.EMPLOYEE, "yes", "bomb", "job_title"))
+    # db.exec(Query.DELETE(Entity.STATE, "state_name='bongoStated'"))
+    # db.exec(Query.DELETE(Entity.STATE, "state_name='bongoStated'"))
+    # db.exec(Query.CREATE(Entity.STATE, "bongoStated", "456"))
+    # db.exec(Query.UPDATE(Entity.STATE, "state_name='bongoStated'", "tax_rate=753.12"))
+    # db.exec(Query.SELECT(Entity.INSURNACE_PLAN, "*"))
+    # db.exec(Query.SELECT(Entity.EMPLOYEE, "first_name", "last_name", "job_title"))
+    # db.exec(Query.CREATE(Entity.EMPLOYEE, "yes", "bomb", "job_title"))
     db.close()
 
 
