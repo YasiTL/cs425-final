88fb031b29fdd6a3b81a3c152d62e3dd14f28e31
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-05-04 16:19:24 -0500

    basic signin module

--- signin.py
+++ signin.py
@@ -10,11 +10,21 @@ class signIn:
         self.db = database
         self.jobTitle = None
 
-    def signIn(self, id: str):
-        self.db.exec(Query.SELECT_WHERE(Entity.EMPLOYEE, "e_id='{}'".format(id)))
+    def signIn(self, first_name: str, e_id: str):
+        """ Sign in given first name and e_id of employee """
+        self.db.exec(Query.SELECT_WHERE(Entity.EMPLOYEE, "first_name='{}' and e_id='{}'".format(first_name, e_id), "job_title"))
+        try:
+            self.jobTitle = DataType.JobTitle(self.db.result()[0][0])
+        except IndexError:
+            self.jobTitle = None
 
     def signOut(self):
+        """ Sign out """
         self.jobTitle = None
 
     def getTitle(self):
+        """Get the title of who is currently signed in, used for getting access level
+        
+        Returns `None` if employee does not exist, or no one is signed in
+        """
         return self.jobTitle
