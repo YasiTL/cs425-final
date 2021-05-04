67e9da461e60b61969bf69c9c0bad8b20185b685
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-05-04 17:06:50 -0500

    update test

--- connTest.py
+++ connTest.py
@@ -1,22 +1,23 @@
 import sys
-from postGres import PostGresDB, Query, Entity, Multivalue, Relation
+from postGres import Query, Entity, Multivalue, Relation
+import postGres as DB
 
 
 def main():
     if len(sys.argv) < 2:
         exit("No ip given as argument")
 
-    db = PostGresDB(sys.argv[1], "Payroll")
-    # db.exec(Query.DELETE(Entity.STATE, "state_name='bongoStated'"))
-    # db.exec(Query.DELETE(Entity.STATE, "state_name='bongoStated'"))
-    # db.exec(Query.CREATE(Entity.STATE, "bongoStated", "456"))
-    # db.exec(Query.UPDATE(Entity.STATE, "state_name='bongoStated'", "tax_rate=753.12"))
-    db.exec(Query.SELECT(Entity.INSURNACE_PLAN, "*"))
-    print(db.result()) #returns python object, a list
-    # db.exec(Query.SELECT(Entity.EMPLOYEE, "first_name", "last_name", "job_title"))
-    # db.exec(Query.CREATE(Entity.EMPLOYEE, "yes", "bomb", "job_title"))
-    db.close()
-    #figure out sql statement for report?
+    DB.connect(sys.argv[1], "Payroll")
+    # DB.execute(Query.DELETE(Entity.STATE, "state_name='bongoStated'"))
+    # DB.execute(Query.DELETE(Entity.STATE, "state_name='bongoStated'"))
+    # DB.execute(Query.CREATE(Entity.STATE, "bongoStated", "456"))
+    # DB.execute(Query.UPDATE(Entity.STATE, "state_name='bongoStated'", "tax_rate=753.12"))
+    DB.execute(Query.SELECT(Entity.INSURNACE_PLAN, "*"))
+    print(DB.result())  # returns python object, a list
+    # DB.execute(Query.SELECT(Entity.EMPLOYEE, "first_name", "last_name", "job_title"))
+    # DB.execute(Query.CREATE(Entity.EMPLOYEE, "yes", "bomb", "job_title"))
+    DB.close()
+    # figure out sql statement for report?
 
 
 main()
