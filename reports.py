1f4f3181a5b858d5f3eac71b908ec15c6fa942a9
aanddrea <a.ocamp3@gmail.com>
2021-05-06 01:51:49 -0500

    temp reports

--- reports.py
+++ reports.py
@@ -5,7 +5,7 @@ from Employee import Employee
 from Dependent import Dependent
 
 
-def Report1():
+def checkReport():
 
     DB.execute(Query.SELECT(Entity.EMPLOYEE, "e_id"))
     for ID in DB.result():
@@ -17,3 +17,13 @@ if len(sys.argv) < 2:
     exit("No ip given as argument")
 
 DB.connect(sys.argv[1], "Payroll")
+
+def w2Report():
+    print("this is report 2")
+    DB.execute(Query.SELECT(Entity.EMPLOYEE, "e_id"))
+    for ID in DB.result():
+        e = Employee(ID[0])
+        print(e.first_name, e.ssn, e.rate)
+
+def expenseReport():
+    print("this is report 3")
