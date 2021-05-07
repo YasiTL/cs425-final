29cbbbb2a8c10fef79c8d18abe2b2548610b77c2
LeHuman <47549331+LeHuman@users.noreply.github.com>
2021-05-06 20:03:26 -0500

    remove comments

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from Dependent import Dependent
 import GUI
 import reports
 import Log
-# import expenseReport
 
 
 def main():
@@ -15,56 +14,9 @@ def main():
         exit("No ip given as argument")
 
     Log.init()
-
     DB.connect(sys.argv[1], "Payroll")
 
-    # print(expenseReport.report())
-
     GUI.init()
-
-    # d0 = Dependent("someDep")
-    # d0.first_name = "whahahah"
-    # d0.update()
-
-    # Auth.signIn("Yasi", 1234567)
-
-    # clearance = Auth.getTitle()
-
-    # if clearance == DataType.JobTitle.ADMIN:
-    #     print("is Admin")
-
-    # e2 = Employee(111111)
-    # e2.ssn = "4563786"
-    # print(e2.Dependents)
-
-    # e1 = Employee(1234567)
-
-    # e1.addPhoneNumber(76)
-    # e1.addPhoneNumber(46478)
-    # e1.addBenefit(DataType.BenefitSelection.DENTAL)
-    # e1.addDependent("someDep")
-    # e1.addDependent("2222222")
-
-    # e1.update()
-
-    # e0 = Employee(
-    #     "newBoi",
-    #     "yakuza",
-    #     "boiboi",
-    #     4567,
-    #     DataType.JobTitle.EMPLOYEE,
-    #     DataType.Salary.HOURLY,
-    #     "basic health",
-    #     "bonga@bonga.com",
-    #     "yes",
-    #     "Illinois",
-    #     "what",
-    #     773302,
-    #     21,
-    # )
-    # e0.create()
-    # e0.first_name = "newName"
-    # e0.update()
 
 
 main()
